5e3ebccc0dadca900a1026d72286f9b38ee1012b
fkrabdillah17 <fkr.abdillah17@gmail.com>
2022-03-28 13:58:15 +0700

    Enter

--- resources/views/layouts/include/header.blade.php
+++ resources/views/layouts/include/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
         <a class="navbar-brand " href="#">
-        <img class="navbar-brand" src="{{ asset('assets/files/pictures/logo.png') }}" alt="..."/>
+            <img class="navbar-brand" src="{{ asset('assets/files/pictures/logo.png') }}" alt="..." />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,13 +11,10 @@
             <ul class="navbar-nav text-center">
 
                 <li class="nav-item">
-                    <a class="nav-link active"href="{{ route('home') }}" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" href="{{ route('home') }}" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Product&Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Team</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Blog</a>
